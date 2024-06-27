d8e20d3c095e455b171a9836aad0d306d71fd266
Viktoriia Ivanets <viktoriyaivanets0@gmail.com>
2024-06-27 14:19:30 +0300

    Fixed search instead of ajax

--- index.php
+++ index.php
@@ -2,52 +2,63 @@
 
 include_once('init.php');
 
-//Search mode
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// Initial default values
+$pageTitle = 'Weather';
+$content = '';
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	// Handle form submission for search
 	$city = htmlspecialchars($_POST['city']);
-
-	//Get search info
-	$searchInfo = getSearchInfo(API_KEY, $city);
-
-	//Define page title
-	$pageTitle = 'Search for ' . $_POST['city'];
-
-	// Template for search results page
-	$content = template('search', [
-		'search_items' => $searchInfo
-	]);
+	if (!empty($city) && !empty(getSearchInfo(API_KEY, $city))) {
+		header('Location: index.php?search=true&city=' . urlencode($city));
+	} else {
+		header('Location: index.php?search=true');
+	}
+	exit(); // Ensure script stops after redirection
 } else {
+
 	// Days for forecast request - default 5
 	$forecast_days = isset($_GET['forecast_days']) ? $_GET['forecast_days'] : 5;
 
 	// City for weather request - default Chernihiv
-	$city = isset($_GET['city']) ? $_GET['city'] : 'Chernihiv';
+	$city = isset($_GET['city']) && !empty($_GET['city']) ? $_GET['city'] : 'Chernihiv';
 
 	// Get full weather info
 	$forecastWeather = getForecastAndCurrentWeather(API_KEY, $forecast_days, $city);
 
-	// If date mode
-	if (isset($_GET['date'])) {
+	if (isset($_GET['search']) && $_GET['search'] == 'true') {
+		if (!isset($_GET['city'])) {
+			$searchInfo = []; // Empty search results
+		}
+		// Get search info
+		$searchInfo = getSearchInfo(API_KEY, $_GET['city']);
 
-		//Get weather info for past day
+		// Define page title
+		$pageTitle = 'Search for ' . htmlspecialchars($_GET['city']);
+
+		// Template for search results page
+		$content = template('search', [
+			'search_items' => $searchInfo
+		]);
+	} elseif (isset($_GET['date'])) {
+		// If date mode
+		// Get weather info for past day
 		if ($_GET['date'] < date("Y-m-d")) {
 			$forecastWeather = getHistoryInfo(API_KEY, $city, $_GET['date']);
 		}
 
-		//Get weather info for future day
+		// Get weather info for future day
 		if ($_GET['date'] > date("Y-m-d", strtotime("+2 weeks"))) {
 			$forecastWeather = getFutureInfo(API_KEY, $city, $_GET['date']);
 		}
 
 		// Define a page title
-		$pageTitle = 'Weather on ' . $_GET['date'];
+		$pageTitle = 'Weather on ' . htmlspecialchars($_GET['date']);
 
 		// Get per hour weather for a full day
 		$perHourWeather = getPerHourWeather($forecastWeather, $_GET['date']);
 
-		// Get avg params 
+		// Get avg params
 		$avgParamsForDay = getAverageWeatherParamsForDay($forecastWeather, $_GET['date']);
 
 		// Template for full day page
@@ -57,16 +68,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			'dateTime' => formatDate($_GET['date']),
 			'avgParamsForDay' => $avgParamsForDay
 		]);
-	}
-
-	// Else default mode
-	else {
-
-		// Forecast days param for full day url
+	} else {
+		// Else default mode
+		// Forecast days param for full day URL
 		$forecast_days_param = $forecast_days;
 		if (str_contains($_SERVER['REQUEST_URI'], 'forecast_days')) {
-			$forecast_days_param =
-				getForecastDaysFromUri($_SERVER['REQUEST_URI']);
+			$forecast_days_param = getForecastDaysFromUri($_SERVER['REQUEST_URI']);
 		}
 
 		// Get astro info for current day
@@ -77,9 +84,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 		// Get chance of rain for current hour
 		$chanceOfRain = getChanceOfRainForCurrentHour(getPerHourWeather($forecastWeather, date("Y-m-d")));
-
-		// Define page title
-		$pageTitle = 'Weather';
 
 		// Template for default page
 		$content = template('default', [
